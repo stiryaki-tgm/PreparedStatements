d535d3fe3a67920efab6b8ad7de96f5058f3b9ac
stiryaki-tgm <seyyid.tiryaki@gmail.com>
2016-04-29 23:11:49 +0200

    main changed to static

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,7 @@
 public class Main {
 
-	private Parser parser;
 
-	private CRUD cRUD;
-
-	private Connector connector;
-
-	public void main(String[] args) {
+	public static void main(String[] args) {
 
 	}
 
