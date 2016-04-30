2a2392a22256f94d6a55f13e6fe909528218d7ad
stiryaki-tgm <seyyid.tiryaki@gmail.com>
2016-04-30 16:01:43 +0200

    main method finished

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,17 @@ public class Main {
 		Parser p = new Parser(args);
 		Connector con = new Connector(p.getHost(), p.getPort(), p.getDB(), p.getUser(), p.getPassword());
 		con.connect();
+
+		CRUD crud = new CRUD(con);
+
+		System.out.println("Verbindung erfolgreich!\nDatensätze werden eingetragen...");
+
+		for(int i=0;i<10000;i++)
+		{
+			//nummer,bezeichnung,gewicht
+			crud.insertProdukt(i,"ProduktNr." + i,1);
+		}
+		System.out.println("Datensätze wurden eingetragen!");
 	}
 
 }
