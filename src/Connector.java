a218d5497e8edc37a22d9a26242f737096f84b7e
stiryaki-tgm <seyyid.tiryaki@gmail.com>
2016-04-30 16:24:12 +0200

    Connector finished

--- src/Connector.java
+++ src/Connector.java
@@ -2,29 +2,55 @@ import org.postgresql.ds.PGSimpleDataSource;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class Connector {
 
 	private PGSimpleDataSource ds;
-
 	private Connection con;
 
-	private Main main;
 
 	public Connector(String host, int port, String database, String user, String password) {
+		ds = new PGSimpleDataSource();
 
+        ds.setServerName(host);
+        ds.setPortNumber(port);
+
+        ds.setDatabaseName(database);
+        ds.setUser(user);
+        ds.setPassword(password);
 	}
 
 	public void connect() {
+        try {
 
+            con = ds.getConnection();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
 	}
 
 	public void closeConnection() {
+        try {
 
+            con.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(-1);
+
+        }
 	}
 
 	public PreparedStatement prepareStmnt(String stm) {
-		return null;
+        try {
+            return con.prepareStatement(stm);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
 	}
 
 }
